8b27c5fdf5c2c1622604d468c15a97539f145b9d
Karen-N-l <karenloti45@gmail.com>
2023-03-16 10:49:10 +0300

    first commit

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    var x:Int
+    var y:Float
+    var w:Double
+    var m:Char
+    var v:String
+    var i:Boolean
+    var answer:Double
+    var age:Int
+    var name:String
+
+    x=10
+    y=20.0f
+    w=30.0
+    m='A'
+    v="Karen"
+    i=true
+
+
+    answer=y+w
+    println(answer)
+
+    //Concatenation
+    age=100
+    name="King"
+    println("Hello "+name+",you are "+age+" years old")
+    println("Hello $name,you are $age years old")
+
+    var firstNum= 20
+    var secodNum= 30
+    var jibu= firstNum + secodNum
+    println("Your answer is $jibu")
+
+
+
+}
